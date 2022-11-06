f8cadce80228da1d8fff961f9a5779176fb74334
Thai Tai Tran <thaitaitran@icloud.com>
2022-11-06 20:51:06 +0100

    split TimerState to TimerInitial and TimerRunning

--- lib/timer/timer_cubit.dart
+++ lib/timer/timer_cubit.dart
@@ -4,17 +4,22 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import './ticker.dart';
 import 'package:equatable/equatable.dart';
 
-class TimerState extends Equatable {
+abstract class TimerState extends Equatable {
   const TimerState({required this.checkIn, required this.checkedInSeconds});
   final DateTime checkIn;
   final int checkedInSeconds;
 
-  TimerState copyWith({required int seconds}) {
-    return TimerState(checkIn: checkIn, checkedInSeconds: seconds);
-  }
-
   @override
   List<Object> get props => [checkIn, checkedInSeconds];
+}
+
+class TimerInitial extends TimerState {
+  TimerInitial() : super(checkIn: DateTime.now(), checkedInSeconds: 0);
+}
+
+class TimerRunning extends TimerState {
+  const TimerRunning(checkIn, checkedInSeconds)
+      : super(checkIn: checkIn, checkedInSeconds: checkedInSeconds);
 }
 
 class TimerCubit extends Cubit<TimerState> {
@@ -22,14 +27,15 @@ class TimerCubit extends Cubit<TimerState> {
   StreamSubscription<int>? _tickerSubscription;
   TimerCubit({required Ticker ticker})
       : _ticker = ticker,
-        super(TimerState(checkIn: DateTime.now(), checkedInSeconds: 0));
+        super(TimerInitial());
 
   void startTimer() {
-    emit(TimerState(checkIn: DateTime.now(), checkedInSeconds: 0));
+    emit(TimerInitial());
     _tickerSubscription?.cancel();
     _tickerSubscription = _ticker.tick().listen((_) {
       final now = DateTime.now();
-      emit(state.copyWith(seconds: now.difference(state.checkIn).inSeconds));
+      emit(
+          TimerRunning(state.checkIn, now.difference(state.checkIn).inSeconds));
     });
   }
 }
